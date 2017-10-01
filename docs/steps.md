0f2d7cd69b31dd2845e39fab6c405e35e20f7bc3
Robin Vercammen <robinvercammen1993@hotmail.com>
2017-10-01 15:31:56 +0200

    menu

--- docs/steps.md
+++ docs/steps.md
@@ -2,3 +2,7 @@
 `npm i -g create-react-app`
 2. init folder
 `create-react-app .`
+3. install material desgin
+`npm i -S material-ui`
+4. roboto font
+`toevoegen aan index`
--- src/client/src/common/Layout.jsx
+++ src/client/src/common/Layout.jsx
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem'
 
 export default class Layout extends Component {
+    constructor() {
+        super();
+        this.state = { open: false }
+    }
+    toggleDrawer = () => {
+        this.setState({ open: !this.state.open });
+    }
+    close = () => {
+        this.setState({ open: false });
+    }
     render() {
-        return <RaisedButton label="Default" />
+        return (
+            <div>
+                <AppBar
+                    title="Dashboard"
+                    onLeftIconButtonTouchTap={this.toggleDrawer}
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                />
+                <Drawer open={this.state.open}>
+                    <MenuItem onClick={this.close}>Dashboard</MenuItem>
+                    <MenuItem onClick={this.close}>Weight</MenuItem>
+                    <MenuItem onClick={this.close}>Calories</MenuItem>
+                    <MenuItem onClick={this.close}>Habits</MenuItem>
+                    <MenuItem onClick={this.close}>Settings</MenuItem>
+                </Drawer>
+            </div>
+        )
     }
 }
